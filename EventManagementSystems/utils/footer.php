6b3b8a0548ac1c2ee4a4a07d253cef38e04df1c8
Dikshak bobade <125991467+dikshakbobade24@users.noreply.github.com>
2023-02-25 16:22:57 +0530

    Add files via upload

---
+++ EventManagementSystems/utils/footer.php
@@ -0,0 +1,30 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>G</strong><span class = "small footerSubtext">overnment</span>
+                        <strong>P</strong><span class = "small footerSubtext">olytechnic</span>
+                        <strong>N</strong><span class = "small footerSubtext">agpur</span>
+                        
+                    </p>
+
+                    <p class = "footerSubtext2">
+                    Our college Mission is to impart quality technical education and higher moral ethics associated with skilled training to suit the modern day technology with innovative concepts,so as to learn to lead the future with full confidence
+                  .
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                <p> <br>
+                 Address: Mangawalri bazar, Sadar, Nagpur
+                    <br>India</p>
+                </div>
+            </section>
+           
+        </div>
+    </div>
+</footer>
---
+++ EventManagementSystems/utils/header.php
@@ -0,0 +1,47 @@
+<title>College Event Handler</title>
+<style>
+.bgImage {
+    background-image: url(images/Banner.jpg);
+    background-size: cover;
+    background-position: center center;
+    height: 717px;
+    margin-bottom: 25px;
+}
+</style>
+<header class="bgImage" > 
+    <nav class="navbar" >
+        <div class="container">
+        <div class="navbar-header"><!--website name/title-->
+               
+                <a class = "navbar-brand">
+                   <h2>CEH</h2>
+                </a>
+        </div>
+       
+            <ul class="nav navbar-nav navbar-right"><!--navigation-->
+                    <li><a href = "index.php"><strong>Home</strong></a></li>
+                    <li><a href = "register.php"><strong>Register</strong></a></li>
+                    <li><a href = "contact.php"><strong>Contact Us</strong></a></li>
+                    <li><a href = "aboutus.php"><strong>About us</strong></a></li>
+                    <li class="btnlogout"><a class = "btn btn-default navbar-btn" href = "login_form.php">Login <span class = "glyphicon glyphicon-log-in"></span></a></li>
+
+                    <!-- <input type="button" onClick="document.getElementById('middle').scrollIntoView();" /> -->
+            
+                
+                
+            </ul>
+        </div><!--container div-->
+    </nav>
+    <div class = "col-md-12">
+        <div class = "container">
+            <div class = "jumbotron"><!--jumbotron-->
+                <h1><strong>College Event Handler<br></strong> 
+                <p style="font-style: bold">"Limitation-It's just your imagination,so just stop thinking about limitation and think about your goal and run towards it to acheive the peak of your goal:)"</p>
+                <br>
+                <br><div class="browse d-md-flex col-md-14" >
+                <div class="row">
+                  
+            </div>
+        </div>
+    </div>
+</header>
